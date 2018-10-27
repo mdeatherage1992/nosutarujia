23ee83314dfc6316dfee7424e099c7725756c285
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-10-27 08:59:09 -0400

    adding json fetches for levels to improve speed and scale for levels

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,19 @@
 import SpriteSheet from './SpriteSheet.js';
-import { loadImage } from './loaders.js';
+import { loadImage, loadLevel } from './loaders.js';
 
+
+function drawBackground(background,context,sprites) {
+  background.ranges.forEach(([x1,x2,y1,y2]) => {
+    for(let x = x1; x < x2; ++x) {
+      for(let y = y1; y < y2; ++y ) {
+        sprites.drawTile(background.tile, context, x, y);
+      }
+    }
+  })
+}
 const canvas = document.getElementById("screen");
 const context = canvas.getContext('2d');
 
-context.fillRect(0,0,50,50);
 
 loadImage('/img/tiles.png')
 .then(image => {
@@ -12,11 +21,12 @@ loadImage('/img/tiles.png')
   sprites.define('ground', 0, 0);
   sprites.define('sky', 3, 23);
 
-  for(let x = 0; x < 25; ++x) {
-    for(let y = 0; y < 14; ++y ) {
-      sprites.drawTile('sky', context, x, y);
-    }
-  }
+  loadLevel('1-1')
+  .then(level => {
+    console.log(level)
+    drawBackground(level.backgrounds[0],context,sprites);
+  });
+
 
   for(let x = 0; x < 25; ++x) {
     for(let y = 12; y < 14; ++y ) {
